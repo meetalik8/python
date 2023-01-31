c7df4627a38c54037c8de8c2f0fd878bfebca761
Meetali <102135323+meetalik8@users.noreply.github.com>
2023-01-31 17:41:25 +0530

    Create 3. productoflist.py

---
+++ functions/3. productoflist.py
@@ -0,0 +1,8 @@
+#3
+def multi_list():
+    result=1
+    for num in num_list:
+        result=result*num
+    return print("The product of elements in list is",result)
+num_list=[1,2,3,4,5]
+multi_list()
