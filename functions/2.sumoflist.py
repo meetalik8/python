16f3b152025b1f224989aef30ac7ec63f3fd6ed5
Meetali <102135323+meetalik8@users.noreply.github.com>
2023-01-31 17:41:01 +0530

    Create 2.sumoflist.py

---
+++ functions/2.sumoflist.py
@@ -0,0 +1,8 @@
+#2
+def sum_list():
+    result=0
+    for num in num_list:
+        result=result+num
+    return print("The sum of elements in the list is ",result)
+num_list=[1,2,3,4,5,6]
+sum_list()
